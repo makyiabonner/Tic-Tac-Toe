cf6825c6960a0fd9e5b43a670a378df42057e411
makyia bonner <makyiabonner@gmail.com>
2023-02-28 14:29:19 -0500

    Writing game logic breakdown

---
+++ index.js
@@ -0,0 +1,25 @@
+//user 0 cpu 0
+//1     2       3
+//4     5       6
+//7     8       9
+
+
+//if user pics a number, the cpu can't pic that number anymore.
+//if cpu pics a number, the user can't pic that number anymore.
+
+/*winning combinations:
+    1,5,9
+    3,5,7
+    7,8,9
+    4,5,6
+    1,2,3
+    1,4,7
+    2,5,8
+    3,6,9
+*/
+
+//if user reaches any winning conditions before cpu, cpu loses
+//if cpu reaches any winning conditions before user, user loses
+
+//make changes to scoreboard
+//after game is over should be a option to retry again
